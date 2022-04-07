2c16f2885d13e09e3db6801cc9181b23d3c9c310
GodwinAkpah <gakpah96@gmail.com>
2022-04-07 14:09:31 +0100

    integrates dashboards

--- resources/views/dashboards/users/index.blade.php
+++ resources/views/dashboards/users/index.blade.php
@@ -1,26 +1,5 @@
-@extends('layouts.app')
-
+@extends('dashboards.users.layouts.user-dash-layout')
+@section('title','Dashboard')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-                <h4> hi user :{{ Auth::user()->name }}</h4>
-                <hr>
-                DAshboard
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+heeer ----
 @endsection
